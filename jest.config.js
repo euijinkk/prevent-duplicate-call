dd1e1b319df2f00992aa50779d308616fb1e3fde
euijinkk <gytks4@naver.com>
2024-04-14 16:00:42 +0900

    chore: jest, rtl setting

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  transform: {
+    // Ensure that both TypeScript and JavaScript files are transformed
+    "^.+\\.(ts|tsx|js|jsx)$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+  },
+};
