12235823372e7d233838720c7d8fac2d4625ac6d
euijinkk <gytks4@naver.com>
2024-04-14 19:04:41 +0900

    chore: cypress 세팅

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+    specPattern: "cypress/component/**/*cy.{js,jsx,ts,tsx}",
+  },
+});
--- jest.config.js
+++ jest.config.js
@@ -11,4 +11,5 @@ module.exports = {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
   },
+  testMatch: ["**/*spec.(ts|tsx|js|jsx)"],
 };
